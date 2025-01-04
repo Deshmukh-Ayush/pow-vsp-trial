7584a96772e5ce3a7c10b50f136f83dff576d809
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2025-01-05 02:22:58 +0530

    multer configured

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,55 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
 
 const app = express();
 
+//multer middleware
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname));
+  },
+});
+
+//multer configurtion
+const upload = multer({
+  storage: storage,
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With",
+    "Content-Type",
+    "Accept"
+  );
+}, next());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
+
 app.get("/", (req, res) => {
-    res.send("Server is working")
+  res.json({ message: "Working fine" });
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    console.log("file uploaded");
 })
 
 app.listen(8000, () => {
-    console.log(`Server is running on port 8000`)
-})
+  console.log(`Server is running on port 8000`);
+});
